e1d8d782d8296a46c849f4979186990fbbce8e6d
OPTYCYK <matleyt@googlemail.com>
2025-12-11 16:22:39 +0000

    Add webpack.config.js for Phaser 3 game bundling and development server setup

---
+++ webpack.config.js
@@ -0,0 +1,155 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const isDevelopment = !isProduction;
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      chunkFilename: isProduction ? '[name].[contenthash].js' : '[name].js',
+      clean: true,
+    },
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 8080,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
+      watchFiles: ['src/**/*', 'public/**/*'],
+      client: {
+        logging: 'info',
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          parser: {
+            dataUrlCondition: {
+              maxSize: 8 * 1024, // 8kb
+            },
+          },
+          generator: {
+            filename: 'images/[name].[hash][ext]',
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name].[hash][ext]',
+          },
+        },
+        {
+          test: /\.(mp3|wav|ogg|m4a)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'audio/[name].[hash][ext]',
+          },
+        },
+        {
+          test: /\.(json)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'data/[name].[hash][ext]',
+          },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        } : false,
+      }),
+      ...(isProduction ? [
+        new InjectManifest({
+          swSrc: './src/service-worker.js',
+          swDest: 'service-worker.js',
+          exclude: [/\.map$/, /hot-update/],
+        }),
+      ] : []),
+    ],
+    resolve: {
+      extensions: ['.js', '.json'],
+      alias: {
+        '@': path.resolve(__dirname, 'src/'),
+        '@scenes': path.resolve(__dirname, 'src/scenes/'),
+        '@objects': path.resolve(__dirname, 'src/objects/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
+        '@assets': path.resolve(__dirname, 'public/assets/'),
+      },
+    },
+    optimization: {
+      minimize: isProduction,
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            priority: 10,
+            reuseExistingChunk: true,
+          },
+          phaser: {
+            test: /[\\/]node_modules[\\/]phaser[\\/]/,
+            name: 'phaser',
+            priority: 20,
+            reuseExistingChunk: true,
+          },
+          common: {
+            minChunks: 2,
+            priority: 5,
+            reuseExistingChunk: true,
+          },
+        },
+      },
+      runtimeChunk: 'single',
+    },
+    performance: {
+      hints: isProduction ? 'warning' : false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+  };
+};
